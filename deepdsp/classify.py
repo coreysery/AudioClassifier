e9b7b1a9be0d3c436a0c3ee349dd837e94f7b3a4
John Ottenlips <john@jammber.com>
2016-10-31 16:15:29 -0500

    testing, trainging, validation matrices

--- deepdsp/classify.py
+++ deepdsp/classify.py
@@ -13,6 +13,9 @@ def main():
 
     # All tracks in a matrix
     audio_matrix = np.zeros((0,88200))
+    testing_matrix = np.zeros((0, 88200))
+    validation_matrix = np.zeros((0, 88200))
+    training_matrix = np.zeros((0, 88200))
     # Each track classified
     classifications = np.zeros((0, len(samples)))
 
@@ -24,11 +27,20 @@ def main():
         classifications = np.vstack((classifications,class_row ))
 
         # append each track to the audio matrix
-        for track in list:
+        for j, track in enumerate(list):
+            # 20% go to testing and validation
+            # 80 to training
+            if(j%10==0):
+                testing_matrix = np.vstack((testing_matrix, track.signal.reshape((1,88200))))
+            elif(j%10==1):
+                validation_matrix = np.vstack((validation_matrix, track.signal.reshape((1,88200))))
+            else:
+                training_matrix = np.vstack((training_matrix, track.signal.reshape((1,88200))))
             audio_matrix = np.vstack((audio_matrix, track.signal.reshape((1,88200))))
 
 
     track_count, sample_length = audio_matrix.shape
+
 
 
 
