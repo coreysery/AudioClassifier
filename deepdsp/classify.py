bbeef1a00b9ad6f7d91b63d853b568fa823528da
Corey Sery <coreysery@gmail.com>
2016-11-01 08:04:29 -0500

    Found better way to randomize audio and seperate into training/testing/validation groups

--- deepdsp/classify.py
+++ deepdsp/classify.py
@@ -3,46 +3,59 @@ import numpy as np
 import tensorflow as tf
 
 from .helpers import loadAudio
+from random import shuffle
+
 
 def main():
-
-    samples = dict(
-        kick = loadAudio('kick'),
-        snare = loadAudio('snare'),
+    library = dict(
+        kick=loadAudio('kick'),
+        snare=loadAudio('snare'),
     )
 
     # All tracks in a matrix
-    audio_matrix = np.zeros((0,88200))
-    testing_matrix = np.zeros((0, 88200))
-    validation_matrix = np.zeros((0, 88200))
-    training_matrix = np.zeros((0, 88200))
-    # Each track classified
-    classifications = np.zeros((0, len(samples)))
+    audio_matrix = np.zeros((0, 88200))
 
-    for i, (kind, list) in enumerate(samples.items()):
+    # Each track classified
+    classifications = np.zeros((0, len(library)))
+
+    for i, (kind, samples_list) in enumerate(library.items()):
+        # Randomize list of audio
+        shuffle(samples_list)
+
         # Set binary classifications for this kind
-        class_row = np.zeros((len(list), len(samples)))
-        ones = np.ones((len(list), 1))
-        class_row[:,i:i+1] = ones
-        classifications = np.vstack((classifications,class_row ))
+        class_row = np.zeros((len(samples_list), len(library)))
+        ones = np.ones((len(samples_list), 1))
+        class_row[:, i:i + 1] = ones
+        classifications = np.vstack((classifications, class_row))
 
         # append each track to the audio matrix
-        for j, track in enumerate(list):
-            # 20% go to testing and validation
-            # 80 to training
-            if(j%10==0):
-                testing_matrix = np.vstack((testing_matrix, track.signal.reshape((1,88200))))
-            elif(j%10==1):
-                validation_matrix = np.vstack((validation_matrix, track.signal.reshape((1,88200))))
-            else:
-                training_matrix = np.vstack((training_matrix, track.signal.reshape((1,88200))))
-            audio_matrix = np.vstack((audio_matrix, track.signal.reshape((1,88200))))
+        for j, track in enumerate(samples_list):
+            audio_matrix = np.vstack((audio_matrix, track.signal.reshape((1, 88200))))
 
 
-    track_count, sample_length = audio_matrix.shape
+    # Randomize Audio
+    combined = list(zip(audio_matrix, classifications))
+    shuffle(combined)
+    audio_matrix[:], classifications[:] = zip(*combined)
 
+    # Ratios
+    ratio_train = .8
+    ratio_test = .1
+    ratio_validation = .1
 
+    # Length of each type according to respective ratios
+    train_len = int(ratio_train * classifications.shape[0])
+    test_len = int(ratio_test * classifications.shape[0])
+    validation_len = int(ratio_validation * classifications.shape[0])
 
+    # Binary Classifications
+    training_labels = classifications[:train_len, :]
+    test_labels = classifications[train_len + 1:train_len + test_len, :]
+    validation_labels = classifications[train_len + test_len + 1:, :]
 
+    # Respective datasets
+    training_matrix = audio_matrix[:train_len, :]
+    testing_matrix = audio_matrix[train_len + 1:train_len + test_len, :]
+    validation_matrix = audio_matrix[train_len + test_len + 1:, :]
 
-
+    # track_count, sample_length = audio_matrix.shape
